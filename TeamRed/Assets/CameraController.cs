c3792569fd86125c9a920e375961f283e08f0bb1
muriel-maths <murielmaths@gmail.com>
2016-03-16 17:58:13 +0100

    Camera controller changes

--- TeamRed/Assets/CameraController.cs
+++ TeamRed/Assets/CameraController.cs
@@ -5,8 +5,11 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        /*
+        float UnitsPerPixel = 1f / 24f;
+        Camera.main.orthographicSize = Screen.height * 4 * UnitsPerPixel;
+        */
+    }
 	
 	// Update is called once per frame
 	void Update () {
