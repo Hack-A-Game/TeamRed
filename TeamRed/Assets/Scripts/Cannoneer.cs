f1de1fed81736f226193b94e9be217d81c363bcc
Jordi Olivares <jordi@jolivar.es>
2016-03-16 19:24:44 +0100

    mierda

--- TeamRed/Assets/Scripts/Cannoneer.cs
+++ TeamRed/Assets/Scripts/Cannoneer.cs
@@ -12,15 +12,4 @@ public class Cannoneer : Character {
         }
     }
 
-    
-
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
