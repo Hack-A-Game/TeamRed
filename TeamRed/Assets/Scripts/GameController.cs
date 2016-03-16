2be5031d17f12a45b82438c2b8d357c87ff26eff
TuTu94 <ctutum@hotmail.com>
2016-03-16 09:49:03 +0100

    Cambio de turnos i contador de tiempo

--- TeamRed/Assets/Scripts/GameController.cs
+++ TeamRed/Assets/Scripts/GameController.cs
@@ -9,23 +9,48 @@ public class GameController : MonoBehaviour {
     public Player player2;
     public Player actualPlayer;
     public MapController mapController;
-
+    public float currentTurnTime;
+    public const float TURN_TIME=30;
 
     void Awake()
     {
         if(instance == null)
         {
             instance = this;
+            currentTurnTime = TURN_TIME;
         }
     }
 
+    //Cambia actualPlayer y reseta el contador de tiempo
+    void ChangeTurn()
+    {
+        if (actualPlayer.CompareTo(player1))
+        {
+            actualPlayer = player2;
+        }
+        else
+        {
+            actualPlayer = player1;
+        }
+        currentTurnTime = TURN_TIME;
+    }
 	// Use this for initialization
 	void Start () {
-	
+        player1 = new Player(1);
+        player2 = new Player(2);
+        actualPlayer = player1; //TODO hacer aleatorio
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        decreaseTurnTime(Time.deltaTime); //Decremento por fotograma
+        if (currentTurnTime <= 0)
+        {
+            ChangeTurn();
+        }
 	}
+    void decreaseTurnTime(float timeDecrease)
+    {
+        currentTurnTime -= timeDecrease; //TODO modificar el contador aqui!!
+    }
 }
