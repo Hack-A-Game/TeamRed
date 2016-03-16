046e35f83293cebf1977b3f5e6634fb2e236ac01
muriel-maths <murielmaths@gmail.com>
2016-03-16 17:47:13 +0100

    Camera fixed

--- TeamRed/Assets/CameraController.cs
+++ TeamRed/Assets/CameraController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraCotroller : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
