8aa6e1ff7f4fe4c403184034dcdb8523aa063e1b
Aitor Mier Pons <aitoremepe@gmail.com>
2016-03-16 11:07:45 +0100

    Assasin Excalibur

---
+++ TeamRed/Assets/Scripts/Excalibur.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Excalibur : Character {
+
+    public override void startVariables()
+    {
+        maxHealth = 99;
+        currentHealth = maxHealth;
+        maxMove = 0;
+        maxAction = 0;
+        costPerAction = 0;
+        costPerMovement = 0;
+        damage = 0;
+        attackRange = 0;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
