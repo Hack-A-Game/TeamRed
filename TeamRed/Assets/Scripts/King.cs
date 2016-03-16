a27756c152700f9bbad6c0ef2e89a246b8681e56
TuTu94 <ctutum@hotmail.com>
2016-03-16 11:04:21 +0100

    king tmp

--- TeamRed/Assets/Scripts/King.cs
+++ TeamRed/Assets/Scripts/King.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class King : Character {
 
-	// Use this for initialization
-	void Start () {
-	
+    public bool hasSword;
+    public override void startVariables()
+    {
+        currentHealth = maxHealth;
+        maxMove = 1;
+
+        //TODO balanceo
+        maxHealth = 50;
+        maxAction = 2; 
+        costPerAction = 4;
+        costPerMovement = 2;
+        damage = 5;
+    }
+
+    public void getSword()
+    {
+        hasSword = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+        hasSword = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
